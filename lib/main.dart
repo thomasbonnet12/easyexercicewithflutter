2a694d8759472d9a48283144e632b068bc9045d4
thomasbonnet12 <thomas.bonnet.12@gmail.com>
2021-01-15 22:19:52 +0100

    button added with annymous function

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,16 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('answer choosen');
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?',
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -17,7 +25,44 @@ class MyApp extends StatelessWidget {
           ),
         ),
         body: Center(
-          child: Text('Body text'),
+          child: Column(
+            children: [
+              Text('question'),
+              SizedBox(height: 20),
+              FlatButton(
+                  child: Text('FlatButton'),
+                  onPressed: () {
+                    answerQuestion();
+                  }),
+              TextButton(
+                  child: Text('New-TextButton'),
+                  onPressed: () {
+                    answerQuestion();
+                  }),
+              SizedBox(height: 30),
+              RaisedButton(
+                  child: Text('RaisedButton'),
+                  onPressed: () {
+                    answerQuestion();
+                  }),
+              ElevatedButton(
+                  child: Text('New-ElevatedButton'),
+                  onPressed: () {
+                    answerQuestion();
+                  }),
+              SizedBox(height: 30),
+              OutlineButton(
+                  child: Text('OutlineButton'),
+                  onPressed: () {
+                    answerQuestion();
+                  }),
+              OutlinedButton(
+                  child: Text('New-OutlinedButton'),
+                  onPressed: () {
+                    answerQuestion();
+                  }),
+            ],
+          ),
         ),
       ),
     );
